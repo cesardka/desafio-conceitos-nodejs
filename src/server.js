8d12b4b77680d3f5b0109e56ae3d3854080f2954
Cesar H <cesardka@hotmail.com>
2020-09-01 01:00:47 -0300

    Adiciona log para quando for iniciar a API
    Vamos apenas informar o local e a porta de acesso para quando for
    iniciar o serviço
    
    Está hardcoded ainda, mas é pra ser a coisinha mais simples mesmo

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
 const app = require("./app");
 
-app.listen(3333);
+app.listen(3333, () => {
+    const hostPort = "3333";
+    const hostAddr = `http://localhost:${hostPort}`;
+    console.log(`Iniciado o serviço no endereço ${hostAddr}`);
+});
