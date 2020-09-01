d91776feffa9dbf490b5975328ce96d6674824ff
Cesar H <cesardka@hotmail.com>
2020-09-01 00:59:51 -0300

    Resolvendo desafio Conceitos NodeJS
    Resolvido o desafio proposto no Desafio 01 do GoStack da Rocketseat,
    conforme instruções no repositório
    https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs
    
    Criada uma API que serve para as seguintes portas:
    
    - GET de todos os repositórios
    - POST de um novo repositório
    - PUT para atualizar um repositório existente
    - DELETE para excluir um repositório existente
    - POST para adicionar o número de curtidas para um repositório existente

--- src/app.js
+++ src/app.js
@@ -1,33 +1,88 @@
-const express = require("express");
-const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const express = require('express');
+const cors    = require("cors");
+const { v4 }  = require('uuid');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
+const logRequests = (request, response, next) => {
+  const { method, url } = request;
+  console.log(`[${method.toUpperCase()}] ${url}`);
+  next();
+}
+app.use(logRequests);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+    const newProject = {
+        id: v4(),
+        title,
+        url,
+        techs,
+        likes: 0,
+    };
+    repositories.push(newProject);
+
+    return response.json(newProject);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if (repositoryIndex < 0) {
+        return response.status(400).json({
+            error: `Project '${id}' could not found`,
+        });
+    }
+
+    const { likes } = repositories[repositoryIndex];
+    const { title, url, techs } = request.body;
+    const newProject = {
+        id,
+        title,
+        url,
+        techs,
+        likes,
+    };
+
+    repositories[repositoryIndex] = newProject;
+
+    return response.json(newProject);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if (repositoryIndex < 0) {
+        return response.status(400).json({
+            error: `Project '${id}' could not found`,
+        });
+    }
+
+    repositories.splice(repositoryIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if (repositoryIndex < 0) {
+        return response.status(400).json({
+            error: `Project '${id}' could not found`,
+        });
+    }
+
+    repositories[repositoryIndex].likes += 1;
+
+    return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
